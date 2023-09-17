5bcbf19100d300cc62e6fde6a3195ac2a44b69c5
Sebastian <mrseb10@gmail.com>
2023-09-17 15:31:38 +0200

    Navbar for Tablets and Mobiles styled

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,26 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import logo from './assets/shared/desktop/logo.svg';
 import cartIcon from './assets/shared/desktop/icon-cart.svg';
+import hamburger from './assets/shared/tablet/icon-hamburger.svg';
 
 const App = () => {
 	return (
 		<main>
 			<BrowserRouter>
 				<header className='bg-stone'>
-					<nav className='xl:px-[165px] flex justify-between items-center py-[35px] border-b-[1px] border-white/40 border-solid'>
-						<Link to='/'>
-							<img src={logo} className='' />
+					<nav className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex justify-between items-center py-[35px] border-b-[1px] border-white/40 border-solid'>
+						<div className='flex items-center xs:justify-between md:justify-normal md:gap-[42px]'>
+							<img src={hamburger} className='lg:hidden cursor-pointer' />
+							{/* Logo when screen-size is larger than xs */}
+							<Link to='/' className='xs:hidden md:flex'>
+								<img src={logo} />
+							</Link>
+						</div>
+						{/* Logo when screen-size is xs */}
+						<Link to='/' className='xs:flex md:hidden'>
+							<img src={logo} />
 						</Link>
-						<div className='text-white flex text-[13px] uppercase font-bold leading-[25px] tracking-[2px] gap-[34px]'>
+						<div className='text-white xs:hidden lg:flex text-[13px] uppercase font-bold leading-[25px] tracking-[2px] gap-[34px]'>
 							<Link
 								to='/'
 								className='hover:text-cream transition-all duration-200'
