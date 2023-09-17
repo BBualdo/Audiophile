900083e073e78a1ffcf83e63178551690fa6e24c
Sebastian <mrseb10@gmail.com>
2023-09-17 16:37:41 +0200

    Menu styled

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,14 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import logo from './assets/shared/desktop/logo.svg';
 import cartIcon from './assets/shared/desktop/icon-cart.svg';
 import hamburger from './assets/shared/tablet/icon-hamburger.svg';
+import headphonesLogo from './assets/shared/desktop/image-category-thumbnail-headphones.png';
+import speakersLogo from './assets/shared/desktop/image-category-thumbnail-speakers.png';
+import earphonesLogo from './assets/shared/desktop/image-category-thumbnail-earphones.png';
+import arrow from './assets/shared/desktop/icon-arrow-right.svg';
 
 const App = () => {
+	const [showMenu, setShowMenu] = React.useState(false);
+
 	return (
 		<main>
 			<BrowserRouter>
@@ -59,6 +65,62 @@ const App = () => {
 							<img src={cartIcon} />
 						</Link>
 					</nav>
+					<div className='bg-white pb-[67px] pt-[108px] px-[40px] flex xs:flex-col md:flex-row xs:gap-[68px] md:gap-[10px] justify-between'>
+						<div className='relative bg-gray-light w-full flex flex-col items-center justify-end pb-[22px] h-[165px] rounded-[8px]'>
+							<img
+								src={headphonesLogo}
+								className='absolute top-[-60px] w-[160px]'
+							/>
+							<div className='flex flex-col items-center gap-[8px]'>
+								<p className='p uppercase font-bold'>Headphones</p>
+								<Link
+									to='/headphones'
+									className='flex items-center gap-[13px] cursor-pointer'
+								>
+									<p className='text-[13px] font-bold opacity-50 uppercase'>
+										Shop
+									</p>
+									<img src={arrow} />
+								</Link>
+							</div>
+						</div>
+						<div className='relative bg-gray-light w-full flex flex-col items-center justify-end pb-[22px] h-[165px] rounded-[8px]'>
+							<img
+								src={speakersLogo}
+								className='absolute top-[-60px] w-[160px]'
+							/>
+							<div className='flex flex-col items-center gap-[8px]'>
+								<p className='p uppercase font-bold'>Speakers</p>
+								<Link
+									to='/speakers'
+									className='flex items-center gap-[13px] cursor-pointer'
+								>
+									<p className='text-[13px] font-bold opacity-50 uppercase'>
+										Shop
+									</p>
+									<img src={arrow} />
+								</Link>
+							</div>
+						</div>
+						<div className='relative bg-gray-light w-full flex flex-col items-center justify-end pb-[22px] h-[165px] rounded-[8px]'>
+							<img
+								src={earphonesLogo}
+								className='absolute top-[-50px] w-[160px]'
+							/>
+							<div className='flex flex-col items-center gap-[8px]'>
+								<p className='p uppercase font-bold'>Earphones</p>
+								<Link
+									to='/earphones'
+									className='flex items-center gap-[13px] cursor-pointer'
+								>
+									<p className='text-[13px] font-bold opacity-50 uppercase'>
+										Shop
+									</p>
+									<img src={arrow} />
+								</Link>
+							</div>
+						</div>
+					</div>
 				</header>
 				<Routes>
 					<Route path='/' element={<Home />} />
