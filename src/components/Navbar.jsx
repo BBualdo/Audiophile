1b5ebba6125d69a6d4784193e393889f8b42e0d5
Sebastian <mrseb10@gmail.com>
2023-09-18 01:38:20 +0200

    Home Page - Menu Component & Navbar Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,8 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/shared/desktop/logo.svg';
 import cartIcon from '../assets/shared/desktop/icon-cart.svg';
 import hamburger from '../assets/shared/tablet/icon-hamburger.svg';
-import headphonesLogo from '../assets/shared/desktop/image-category-thumbnail-headphones.png';
-import speakersLogo from '../assets/shared/desktop/image-category-thumbnail-speakers.png';
-import earphonesLogo from '../assets/shared/desktop/image-category-thumbnail-earphones.png';
-import arrow from '../assets/shared/desktop/icon-arrow-right.svg';
+
+import Menu from './Menu';
 
 const Navbar = () => {
 	const [showMenu, setShowMenu] = React.useState(false);
@@ -31,7 +29,7 @@ const Navbar = () => {
 	}, []);
 
 	return (
-		<nav className='xs:px-[24px] md:px-[40px] lg:px-[165px] bg-stone'>
+		<nav className='fixed top-0 left-0 right-0 xs:px-[24px] md:px-[40px] lg:px-[165px] bg-stone z-50'>
 			<div className='flex justify-between items-center py-[35px]'>
 				<div className='flex items-center xs:justify-between md:justify-normal md:gap-[42px]'>
 					<img
@@ -75,64 +73,7 @@ const Navbar = () => {
 					<img src={cartIcon} />
 				</Link>
 			</div>
-			{showMenu && (
-				<div className='bg-white pb-[67px] pt-[108px] px-[40px] flex xs:flex-col md:flex-row xs:gap-[68px] md:gap-[10px] justify-between'>
-					<div className='relative bg-gray-light w-full flex flex-col items-center justify-end pb-[22px] h-[165px] rounded-[8px]'>
-						<img
-							src={headphonesLogo}
-							className='absolute top-[-60px] w-[160px]'
-						/>
-						<div className='flex flex-col items-center gap-[8px]'>
-							<p className='p uppercase font-bold'>Headphones</p>
-							<Link
-								to='/headphones'
-								className='flex items-center gap-[13px] cursor-pointer hover:text-cream'
-							>
-								<p className='text-[13px] font-bold opacity-50 uppercase'>
-									Shop
-								</p>
-								<img src={arrow} />
-							</Link>
-						</div>
-					</div>
-					<div className='relative bg-gray-light w-full flex flex-col items-center justify-end pb-[22px] h-[165px] rounded-[8px]'>
-						<img
-							src={speakersLogo}
-							className='absolute top-[-60px] w-[160px]'
-						/>
-						<div className='flex flex-col items-center gap-[8px]'>
-							<p className='p uppercase font-bold'>Speakers</p>
-							<Link
-								to='/speakers'
-								className='flex items-center gap-[13px] cursor-pointer hover:text-cream'
-							>
-								<p className='text-[13px] font-bold opacity-50 uppercase'>
-									Shop
-								</p>
-								<img src={arrow} />
-							</Link>
-						</div>
-					</div>
-					<div className='relative bg-gray-light w-full flex flex-col items-center justify-end pb-[22px] h-[165px] rounded-[8px]'>
-						<img
-							src={earphonesLogo}
-							className='absolute top-[-50px] w-[160px]'
-						/>
-						<div className='flex flex-col items-center gap-[8px]'>
-							<p className='p uppercase font-bold'>Earphones</p>
-							<Link
-								to='/earphones'
-								className='flex items-center gap-[13px] cursor-pointer hover:text-cream'
-							>
-								<p className='text-[13px] font-bold opacity-50 uppercase'>
-									Shop
-								</p>
-								<img src={arrow} />
-							</Link>
-						</div>
-					</div>
-				</div>
-			)}
+			{showMenu && <Menu />}
 		</nav>
 	);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import Menu from '../components/Menu';
+
 const Home = () => {
 	return (
-		<>
-			<header className='xs:bg-xs-hero-pattern md:bg-md-hero-pattern lg:bg-lg-hero-pattern bg-no-repeat bg-center bg-cover md:px-[40px] lg:px-[165px] w-full'>
+		<main>
+			<header className='xs:bg-xs-hero-pattern md:bg-md-hero-pattern mt-[96] lg:bg-lg-hero-pattern bg-no-repeat bg-center bg-cover md:px-[40px] lg:px-[165px] w-full'>
 				<div className='border-t-[1px] border-white/40 border-solid pt-[128px] pb-[158px] flex flex-col justify-center xs:items-center xs:text-center lg:items-start lg:text-left'>
 					<div className='w-[398px] h-[346px]'>
-						<p className='text-white font-normal opacity-50 uppercase tracking-[10px]'>
+						<p className='text-white/50 font-normal uppercase tracking-[10px]'>
 							New Product
 						</p>
-						<h1 className='h1 text-white mt-6'>XX99 Mark II Headphones</h1>
+						<h1 className='xs:text-[36px] xs:leading-[40px] xs:tracking-[1.3px] md:text-[56px] md:leading-[58px] md:tracking-[2px] text-white mt-6'>
+							XX99 Mark II Headphones
+						</h1>
 						<p className='mt-6 text-white/75 xs:px-[20px] lg:px-0 lg:w-[360px]'>
 							Experience natural, lifelike audio and exceptional build quality
 							made for the passionate music enthusiast.
@@ -20,7 +24,10 @@ const Home = () => {
 					</div>
 				</div>
 			</header>
-		</>
+			<div className='md:mt-[52px] lg:mt-[92px]'>
+				<Menu />
+			</div>
+		</main>
 	);
 };
 
