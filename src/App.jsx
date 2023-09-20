e8057d2c7e5bc41b3b427604b6369d066cdd1fe4
Sebastian <mrseb10@gmail.com>
2023-09-21 00:00:59 +0200

    Merged categories into 1 Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,59 @@
 import React from 'react';
 
 import Home from './pages/Home';
-import Headphones from './pages/Headphones';
-import Speakers from './pages/Speakers';
-import Earphones from './pages/Earphones';
+import Category from './pages/Category';
 import Cart from './pages/Cart';
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Layout from './components/Layout';
 
 const App = () => {
+	const [categoryData, setCategoryData] = React.useState({
+		headphones: [],
+		speakers: [],
+		earphones: [],
+	});
+
+	React.useEffect(() => {
+		fetch('../../data.json')
+			.then((res) => res.json())
+			.then((data) => {
+				const categorizedData = {
+					headphones: data.filter((item) => item.category === 'headphones'),
+					speakers: data.filter((item) => item.category === 'speakers'),
+					earphones: data.filter((item) => item.category === 'earphones'),
+				};
+				setCategoryData(categorizedData);
+			});
+	}, []);
+
+	const categories = {
+		headphones: {
+			title: 'Headphones',
+			products: categoryData.headphones,
+		},
+		speakers: {
+			title: 'Speakers',
+			products: categoryData.speakers,
+		},
+		earphones: {
+			title: 'Earphones',
+			products: categoryData.earphones,
+		},
+	};
+
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
 					<Route element={<Layout />}>
 						<Route path='/' element={<Home />} />
-						<Route path='/headphones' element={<Headphones />} />
-						<Route path='/speakers' element={<Speakers />} />
-						<Route path='/earphones' element={<Earphones />} />
+						<Route
+							path='/:category'
+							element={
+								<Category category='headphones' categories={categories} />
+							}
+						/>
 						<Route path='/cart' element={<Cart />} />
 					</Route>
 				</Routes>
