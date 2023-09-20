67b5d25f9a9878c699a937ee17d1cb4752b7eed6
Sebastian <mrseb10@gmail.com>
2023-09-20 15:39:14 +0200

    Copy paste to Speakers and Earphones Page

--- src/pages/Earphones.jsx
+++ src/pages/Earphones.jsx
@@ -15,21 +15,48 @@ const Earphones = () => {
 			});
 	}, []);
 
-	const earphonesElements = earphonesData.map((earphone) => {
+	const reversedEarphonesData = [...earphonesData].reverse();
+
+	const earphonesElements = reversedEarphonesData.map((earphone, index) => {
+		const isEvenIndex = index % 2 === 0;
 		return (
-			<div key={earphone.id}>
+			<div
+				className={`${
+					isEvenIndex ? 'lg:flex-row' : 'lg:flex-row-reverse'
+				} flex xs:flex-col items-center xs:gap-8 md:gap-[52px] lg:gap-[125px]`}
+				key={earphone.id}
+			>
 				<div>
-					<img />
+					{/* desktop image */}
+					<img
+						className='xs:hidden lg:block'
+						src={earphone.categoryImage.desktop}
+						alt={earphone.name}
+					/>
+					{/* tablet image */}
+					<img
+						className='xs:hidden md:block lg:hidden '
+						src={earphone.categoryImage.tablet}
+						alt={earphone.name}
+					/>
+					{/* mobile image */}
+					<img
+						className='xs:block md:hidden'
+						src={earphone.categoryImage.mobile}
+						alt={earphone.name}
+					/>
 				</div>
-				<div>
+				<div className='xs:text-center lg:text-left'>
 					{earphone.new && (
 						<p className='text-[14px] text-cream tracking-[10px] font-normal uppercase mb-4'>
 							New Product
 						</p>
 					)}
 					<h2 className='h2'>{earphone.name}</h2>
-					<p className='p mt-8'>{earphone.description}</p>
-					<button className='btn bg-cream hover:bg-cream-light transition-all duration-150 mt-10'>
+					<p className='p text-black/50 xs:mt-6 md:mt-8'>
+						{earphone.description}
+					</p>
+					<button className='btn bg-cream hover:bg-cream-light transition-all duration-150 xs:mt-6 lg:mt-10'>
 						See Product
 					</button>
 				</div>
@@ -39,10 +66,12 @@ const Earphones = () => {
 
 	return (
 		<main>
-			<header className='bg-stone text-white mt-[95px] flex items-center justify-center py-[98px]'>
-				<h2>Earphones</h2>
+			<header className='bg-stone text-white mt-[95px] flex items-center justify-center xs:py-8 md:py-[98px]'>
+				<h2 className='xs:text-[24px] md:text-[40px] font-bold xs:leading-normal md:leading-[44px] xs:tracking-[2px] md:tracking-[1.4px] uppercase'>
+					earphones
+				</h2>
 			</header>
-			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex flex-col gap-[160px] py-[160px]'>
+			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex flex-col xs:gap-[120px] lg:gap-[160px] xs:pt-16 xs:pb-[120px] md:py-[120px] lg:py-[160px]'>
 				{earphonesElements}
 			</section>
 			<Menu />
--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -15,21 +15,48 @@ const Speakers = () => {
 			});
 	}, []);
 
-	const speakersElements = speakersData.map((speaker) => {
+	const reversedSpeakersData = [...speakersData].reverse();
+
+	const speakersElements = reversedSpeakersData.map((speaker, index) => {
+		const isEvenIndex = index % 2 === 0;
 		return (
-			<div key={speaker.id}>
+			<div
+				className={`${
+					isEvenIndex ? 'lg:flex-row' : 'lg:flex-row-reverse'
+				} flex xs:flex-col items-center xs:gap-8 md:gap-[52px] lg:gap-[125px]`}
+				key={speaker.id}
+			>
 				<div>
-					<img />
+					{/* desktop image */}
+					<img
+						className='xs:hidden lg:block'
+						src={speaker.categoryImage.desktop}
+						alt={speaker.name}
+					/>
+					{/* tablet image */}
+					<img
+						className='xs:hidden md:block lg:hidden '
+						src={speaker.categoryImage.tablet}
+						alt={speaker.name}
+					/>
+					{/* mobile image */}
+					<img
+						className='xs:block md:hidden'
+						src={speaker.categoryImage.mobile}
+						alt={speaker.name}
+					/>
 				</div>
-				<div>
+				<div className='xs:text-center lg:text-left'>
 					{speaker.new && (
 						<p className='text-[14px] text-cream tracking-[10px] font-normal uppercase mb-4'>
 							New Product
 						</p>
 					)}
 					<h2 className='h2'>{speaker.name}</h2>
-					<p className='p mt-8'>{speaker.description}</p>
-					<button className='btn bg-cream hover:bg-cream-light transition-all duration-150 mt-10'>
+					<p className='p text-black/50 xs:mt-6 md:mt-8'>
+						{speaker.description}
+					</p>
+					<button className='btn bg-cream hover:bg-cream-light transition-all duration-150 xs:mt-6 lg:mt-10'>
 						See Product
 					</button>
 				</div>
@@ -39,10 +66,12 @@ const Speakers = () => {
 
 	return (
 		<main>
-			<header className='bg-stone text-white mt-[95px] flex items-center justify-center py-[98px]'>
-				<h2>Speakers</h2>
+			<header className='bg-stone text-white mt-[95px] flex items-center justify-center xs:py-8 md:py-[98px]'>
+				<h2 className='xs:text-[24px] md:text-[40px] font-bold xs:leading-normal md:leading-[44px] xs:tracking-[2px] md:tracking-[1.4px] uppercase'>
+					Speakers
+				</h2>
 			</header>
-			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex flex-col gap-[160px] py-[160px]'>
+			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex flex-col xs:gap-[120px] lg:gap-[160px] xs:pt-16 xs:pb-[120px] md:py-[120px] lg:py-[160px]'>
 				{speakersElements}
 			</section>
 			<Menu />
