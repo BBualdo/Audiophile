4f7cb066fd70b73e631714e4495122008d5a0e5d
Sebastian <mrseb10@gmail.com>
2023-09-20 15:33:07 +0200

    Finished Styling for Headphones Page

--- src/pages/Earphones.jsx
+++ src/pages/Earphones.jsx
@@ -15,11 +15,36 @@ const Earphones = () => {
 			});
 	}, []);
 
+	const earphonesElements = earphonesData.map((earphone) => {
+		return (
+			<div key={earphone.id}>
+				<div>
+					<img />
+				</div>
+				<div>
+					{earphone.new && (
+						<p className='text-[14px] text-cream tracking-[10px] font-normal uppercase mb-4'>
+							New Product
+						</p>
+					)}
+					<h2 className='h2'>{earphone.name}</h2>
+					<p className='p mt-8'>{earphone.description}</p>
+					<button className='btn bg-cream hover:bg-cream-light transition-all duration-150 mt-10'>
+						See Product
+					</button>
+				</div>
+			</div>
+		);
+	});
+
 	return (
 		<main>
 			<header className='bg-stone text-white mt-[95px] flex items-center justify-center py-[98px]'>
 				<h2>Earphones</h2>
 			</header>
+			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex flex-col gap-[160px] py-[160px]'>
+				{earphonesElements}
+			</section>
 			<Menu />
 			<BestGear />
 		</main>
--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -15,11 +15,36 @@ const Speakers = () => {
 			});
 	}, []);
 
+	const speakersElements = speakersData.map((speaker) => {
+		return (
+			<div key={speaker.id}>
+				<div>
+					<img />
+				</div>
+				<div>
+					{speaker.new && (
+						<p className='text-[14px] text-cream tracking-[10px] font-normal uppercase mb-4'>
+							New Product
+						</p>
+					)}
+					<h2 className='h2'>{speaker.name}</h2>
+					<p className='p mt-8'>{speaker.description}</p>
+					<button className='btn bg-cream hover:bg-cream-light transition-all duration-150 mt-10'>
+						See Product
+					</button>
+				</div>
+			</div>
+		);
+	});
+
 	return (
 		<main>
 			<header className='bg-stone text-white mt-[95px] flex items-center justify-center py-[98px]'>
 				<h2>Speakers</h2>
 			</header>
+			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex flex-col gap-[160px] py-[160px]'>
+				{speakersElements}
+			</section>
 			<Menu />
 			<BestGear />
 		</main>
