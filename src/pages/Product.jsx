f0b14b9c79a2ea1947bc6e364f8a59b0d1891dde
Sebastian <mrseb10@gmail.com>
2023-09-22 10:19:13 +0200

    Fixed "Go back" button

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -77,11 +77,13 @@ const Product = () => {
 
 	return (
 		<main className='mt-[95px]'>
-			<Link relative='path' to='..'>
-				<p className='p text-black/50 cursor-pointer hover:text-cream transition-all duration-150 xs:px-[24px] md:px-[40px] lg:px-[165px] xs:pt-4 md:pt-8 lg:pt-[79px]'>
-					Go back
-				</p>
-			</Link>
+			<div className='xs:px-[24px] md:px-[40px] lg:px-[165px] xs:pt-4 md:pt-8 lg:pt-[79px]'>
+				<Link relative='path' to='..'>
+					<button className='p text-black/50 cursor-pointer hover:text-cream transition-all duration-150'>
+						Go back
+					</button>
+				</Link>
+			</div>
 
 			<section className='xs:px-[24px] md:px-[40px] lg:px-[165px] flex xs:flex-col md:flex-row xs:items-start md:items-center lg:min-w-[730px] gap-[10%] xs:mt-6 lg:mt-[56px]'>
 				<div>
